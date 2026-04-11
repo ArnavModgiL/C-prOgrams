a692187e1c32ae20bfd02fe7dc23a43d037ecf5d
ARNAV-MODGIL <240049496+ArnavModgiL@users.noreply.github.com>
2026-04-11 15:24:40 +0530

    Question: Count total characters in a file.

---
+++ FILE - HANDLING/FILE3.c
@@ -0,0 +1,20 @@
+// count characters in FILE :
+
+# include<stdio.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+    int count = 0;
+
+    fp = fopen("data.txt","r");
+
+    while((ch = fgetc(fp)) != EOF) {
+        count++;
+    }
+
+    fclose(fp);
+
+    printf("Total characters: %d",count);
+    return 0;
+}
