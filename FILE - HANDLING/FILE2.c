277a647fb46b34d99aab095ddfabb1f5bcff58a9
ARNAV-MODGIL <240049496+ArnavModgiL@users.noreply.github.com>
2026-04-11 15:19:06 +0530

    Question: Copy contents of one file into another

---
+++ FILE - HANDLING/FILE2.c
@@ -0,0 +1,17 @@
+//Copy On File Into Another --:
+
+#include<stdio.h>
+
+int main() {
+    FILE *fp1, *fp2;
+    char ch;
+
+    fp1 = fopen("data.txt","r");
+    fp2 = fopen("copy.txt","w");
+
+    while((ch = fgetc(fp1)) != EOF) {
+        fputc(ch,fp2);
+    }
+    fclose(fp1);
+}
+
